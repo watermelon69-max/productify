0bdf4719a27d71e832bbc4e7542cf020704cb331
watermelon69-max <anuragmourya901@gmail.com>
2026-07-22 10:50:16 +0530

    Add SVG assets for React and Vite logos

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,21 @@
-console.log("Hey from from .ts filer");
+import express from "express"
+import { ENV } from "./config/env"
+import { clerkMiddleware } from "@clerk/express"
+import cors from "cors"
 
-const num1:number=5
-const num2:number=10
+const app = express()
 
-console.log(num1+num2);
+app.use(cors({ origin: ENV.FRONTEND_URL }))
+app.use(clerkMiddleware()); //auth objext will be attached to the request object 
+app.use(express.json())// parses JSON request bodies 
+app.use(express.urlencoded({ extended: true }))
+
+
+app.use("/", (req, res) => {
+    res.send({ success: true })
+})
+
+app.listen(ENV.PORT, () => {
+    console.log("Server is up and listening to port ", ENV.PORT);
+
+})
