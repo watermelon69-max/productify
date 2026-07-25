ee573cbcd8b42e785550908deb4240436f2c8e26
watermelon69-max <anuragmourya901@gmail.com>
2026-07-25 20:42:31 +0530

    fix: change role attribute to type for button in ThemeSelector component

--- frontend/src/components/ThemeSelector.jsx
+++ frontend/src/components/ThemeSelector.jsx
@@ -51,7 +51,7 @@ function ThemeSelector() {
 
   return (
     <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-sm gap-1">
+      <div tabIndex={0} type="button" className="btn btn-ghost btn-sm gap-1">
         <PaletteIcon className="size-4" />
         <span className="hidden sm:inline">Theme</span>
       </div>
